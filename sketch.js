ebf8d9c51274ec2264c45bac4f2ae52637d50c06
Alan Sebastian <68325551+AlanAppuSebastian@users.noreply.github.com>
2021-05-21 16:00:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var waiter,waiterimg;
+var manager,managerimg;
+var cleaner,cleanerimg;
+var bgimg;
+var cloth,clothimg;
+var knife,knifeimg;
+var bag,bagimg;
+var rain_bgimg;
+var Call;
+
+function preload(){
+
+waiterimg=loadImage("ASSETS/WAITER.png");
+managerimg=loadImage("ASSETS/MANAGER.png");
+cleanerimg=loadImage("ASSETS/CLEANER.png");
+clothimg=loadImage("ASSETS/cloth.png");
+knifeimg=loadImage("ASSETS/KNIFE.png");
+bagimg=loadImage("ASSETS/BAG.jpg");
+rain_bgimg=loadImage("ASSETS/RAIN.jpg");
+bgimg=loadImage("ASSETS/bg.jpg");
+Call=loadSound("ASSETS/CALL.mp3");
+}
+
+function setup(){
+  
+}
+
+
+function draw(){
+  
+}
+
+
